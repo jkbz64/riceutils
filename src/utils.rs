c3b08381bc5e8d17988059a4741d0493f0199d1f
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2024-06-30 10:18:31 +0200

    pulse: do not use pamixer

--- src/utils.rs
+++ src/utils.rs
@@ -18,6 +18,36 @@ pub async fn process_signals(child: &mut Child) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub async fn list_sinks() -> Result<Vec<Source>, Box<dyn Error>> {
+    let output = Command::new("pactl")
+        .arg("list")
+        .arg("sinks")
+        .output()
+        .await?;
+    let text = String::from_utf8_lossy(&output.stdout);
+
+    // Parse pactl list sources into Source structs
+    let mut sources = Vec::new();
+
+    let mut name = "".to_string();
+
+    for line in text.lines() {
+        let line = line.trim();
+        if line.starts_with("Name: ") {
+            name = line.split_whitespace().last().unwrap().to_string();
+        }
+
+        if line.starts_with("Mute: ") {
+            sources.push(Source {
+                name: name.clone(),
+                mute: line.contains("Mute: yes"),
+            });
+        }
+    }
+
+    Ok(sources)
+}
+
 pub async fn list_sources() -> Result<Vec<Source>, Box<dyn Error>> {
     let output = Command::new("pactl")
         .arg("list")
@@ -48,6 +78,19 @@ pub async fn list_sources() -> Result<Vec<Source>, Box<dyn Error>> {
     Ok(sources)
 }
 
+pub async fn default_sink() -> Result<String, Box<dyn Error>> {
+    let output = Command::new("pactl").arg("info").output().await?;
+    let text = String::from_utf8_lossy(&output.stdout);
+
+    for line in text.lines() {
+        if line.starts_with("Default Sink:") {
+            return Ok(line.split_whitespace().last().unwrap().to_string());
+        }
+    }
+
+    Err("No default source found".into())
+}
+
 pub async fn default_source() -> Result<String, Box<dyn Error>> {
     let output = Command::new("pactl").arg("info").output().await?;
     let text = String::from_utf8_lossy(&output.stdout);
@@ -63,10 +106,16 @@ pub async fn default_source() -> Result<String, Box<dyn Error>> {
 
 // pamixer: is output muted
 pub async fn is_output_muted() -> Result<bool, Box<dyn Error>> {
-    let output = Command::new("pamixer").arg("--get-mute").output().await?;
-    let text = String::from_utf8_lossy(&output.stdout);
+    let sink = default_sink().await?;
+    let sinks = list_sinks().await?;
 
-    Ok(text.trim() == "true")
+    for s in sinks {
+        if s.name == sink {
+            return Ok(s.mute);
+        }
+    }
+
+    Ok(true)
 }
 
 pub async fn is_input_muted() -> Result<bool, Box<dyn Error>> {
